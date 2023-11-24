6ffe400ab6b29e9fb6f2e0d9841e4245e669e4ba
levitategu <1751143724@qq.com>
2023-11-24 18:26:47 +0800

    Initial Commit

---
+++ miniprogram/config.js
@@ -0,0 +1,156 @@
+var data = {
+      //云开发环境id
+      env: 'cloud1-7gk3st9c62b3263c',
+      //分享配置
+      share_title: '西安电子科技大学二手书',
+      share_img: '/images/poster.jpg', //可以是网络地址，本地文件路径要填绝对位置
+      share_poster:'',//必须为网络地址
+      //客服联系方式
+      kefu: {
+            weixin: '',
+            qq: '',
+            gzh: '', //公众号二维码必须为网络地址
+            phone: '' //如果你不设置电话客服，就留空
+      },
+      //默认启动页背景图，防止请求失败完全空白 
+      //可以是网络地址，本地文件路径要填绝对位置
+      bgurl: '/images/startBg.jpg',
+      //校区
+      campus: [{
+                  name: '西电南校区',
+                  id: 0
+            },
+            {
+                  name: '西电北校区',
+                  id: 1
+            },
+            {
+                  name: '杭研院',
+                  id: 2
+            },
+            {
+                  name: '广研院',
+                  id: 3
+            },
+      ],
+      //配置学院，建议不要添加太多，不然前端不好看
+      college: [{
+                  name: '通用',
+                  id: -1
+            },
+            {
+                  name: '通信',
+                  id: 0
+            },
+            {
+                  name: '电子',
+                  id: 1
+            },
+            {
+                  name: '计算机',
+                  id: 2
+            },
+            {
+                  name: '机电',
+                  id: 3
+            },
+            {
+                  name: '数统',
+                  id: 4
+            },
+            {
+                  name: '物理',
+                  id: 5
+            },
+            {
+                  name: '光电',
+                  id: 6
+            },
+            {
+                  name: '经管',
+                  id: 7
+            },
+            {
+                  name: '人文',
+                  id: 8
+            },
+            {
+                  name: '外国语',
+                  id: 9
+            },
+            {
+                  name: '微电子',
+                  id: 10
+            },
+            {
+                  name: '生科',
+                  id: 11
+            },
+            {
+                  name: '材料',
+                  id: 12
+            },
+            {
+                  name: '空科',
+                  id: 13
+            },
+            {
+                  name: '网信',
+                  id: 14
+            },
+            {
+              name: '人智',
+              id: 15
+            },
+            {
+              name: '马克思',
+              id: 16
+            },
+      ],
+}
+//下面的就别动了
+function formTime(creatTime) {
+      let date = new Date(creatTime),
+            Y = date.getFullYear(),
+            M = date.getMonth() + 1,
+            D = date.getDate(),
+            H = date.getHours(),
+            m = date.getMinutes(),
+            s = date.getSeconds();
+      if (M < 10) {
+            M = '0' + M;
+      }
+      if (D < 10) {
+            D = '0' + D;
+      }
+      if (H < 10) {
+            H = '0' + H;
+      }
+      if (m < 10) {
+            m = '0' + m;
+      }
+      if (s < 10) {
+            s = '0' + s;
+      }
+      return Y + '-' + M + '-' + D + ' ' + H + ':' + m + ':' + s;
+}
+
+function days() {
+      let now = new Date();
+      let year = now.getFullYear();
+      let month = now.getMonth() + 1;
+      let day = now.getDate();
+      if (month < 10) {
+            month = '0' + month;
+      }
+      if (day < 10) {
+            day = '0' + day;
+      }
+      let date = year + "" + month + day;
+      return date;
+}
+module.exports = {
+      data: JSON.stringify(data),
+      formTime: formTime,
+      days: days
+}
