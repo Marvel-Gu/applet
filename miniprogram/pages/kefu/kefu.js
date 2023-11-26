b4202750af08967f9a2bcfd20900bf23c3318b1e
levitategu <1751143724@qq.com>
2023-11-26 12:24:51 +0800

    更新

--- miniprogram/pages/kefu/kefu.js
+++ miniprogram/pages/kefu/kefu.js
@@ -1,44 +1,66 @@
-var app = getApp();
-const config = require("../../config.js");
-
+// pages/kefu/kefu.js
 Page({
 
-      /**
-       * 页面的初始数据
-       */
-      data: {
-            weixin: JSON.parse(config.data).kefu.weixin,
-            gzh: JSON.parse(config.data).kefu.gzh,
-            phone: JSON.parse(config.data).kefu.phone,
-            banner: "/images/kefu.jpg"
-      },
-      onLoad() {
+  /**
+   * 页面的初始数据
+   */
+  data: {
 
-      },
+  },
 
-      //复制
-      copy(e) {
-            wx.setClipboardData({
-                  data: e.currentTarget.dataset.copy,
-                  success: res => {
-                        wx.showToast({
-                              title: '复制' + e.currentTarget.dataset.name+'成功',
-                              icon: 'success',
-                              duration: 1000,
-                        })
-                  }
-            })
-      },
-      //电话拨打
-      phone(e) {
-            wx.makePhoneCall({
-                  phoneNumber: e.currentTarget.dataset.phone
-            })
-      },
-      //预览图片
-      preview(e) {
-            wx.previewImage({
-                  urls: e.currentTarget.dataset.link.split(",")
-            });
-      },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
 })
